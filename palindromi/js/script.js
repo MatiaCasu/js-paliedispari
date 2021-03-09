2036cbc95d4bc3ca583368cd86118888238f3381
Matia <matia.casu@gmail.com>
2021-03-09 22:35:01 +0100

    Corretto ciclo for funzione palindromi

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,26 +1,23 @@
-/* Una funzione per capire se la parola è
-palindroma;*/
-
 // funzione palindromi
 function palindromi(parola){
   var reverse = "";
-
   var i = (parola.length - 1);
-    for(i; i !== -1; i--){
-      parola[i] = i;
-      reverse += parola[i];
-    }
-    if(parola === reverse){
-      return true;
-    }
+  for(i; i !== -1; i--){
+    reverse += parola[i];
+  }
+  if(parola === reverse){
+    return true;
+  }
+  return false;
 }
 
 // variabile input con funzione palindromi
-var input = palindromi(prompt("Inserisci parola!"));
+var input = prompt("Inserisci parola!");
 
-// se palindromi(input) è vera la parola è palindroma 
-if(input){
+// se palindromi(input) è vera la parola è palindroma
+if(palindromi(input)){
   alert("Parola palindroma!");
 }else{
   alert("Parola non palindroma");
 }
+console.log(input);
